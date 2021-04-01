90c9c62ac29e4194cbfd9dae587fdba808d51948
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-03-31 22:06:45 -0300

    feat: add delete routes to category and publisher

--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,10 @@ const publisherController = require('./controllers/PublisherController')
 
 routes.get('/categories', categoryController.list)
 routes.post('/categories', categoryController.create)
+routes.delete('/categories/:categoryId', categoryController.delete)
 
 routes.get('/publishers', publisherController.list)
 routes.post('/publishers', publisherController.create)
+routes.delete('/publishers/:publisherId', publisherController.delete)
 
 module.exports = routes
