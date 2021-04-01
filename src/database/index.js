d6f965e247232e668c400b3aaacb243de93c2d91
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-03-31 21:17:17 -0300

    add category model to connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize')
 const configDB = require('../config/database')
+const Category = require('../models/Category')
 
 const connection = new Sequelize(configDB)
+
+Category.init(connection)
 
 module.exports = connection
