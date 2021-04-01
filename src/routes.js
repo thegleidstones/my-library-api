2966891d307a55a3d50075ccdb0f255b1d7c567a
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-03-31 21:32:26 -0300

    add publishers routes

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,12 @@ const express = require('express')
 const routes = express.Router()
 
 const categoryController = require('./controllers/CategoryController')
+const publisherController = require('./controllers/PublisherController')
 
 routes.get('/categories', categoryController.list)
 routes.post('/categories', categoryController.create)
+
+routes.get('/publishers', publisherController.list)
+routes.post('/publishers', publisherController.create)
 
 module.exports = routes
