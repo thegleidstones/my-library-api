f5665cb3166f79bd7381b1e0efea0e90211b7b55
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-03-31 21:32:56 -0300

    add publisher model to connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize')
 const configDB = require('../config/database')
 const Category = require('../models/Category')
+const Publisher = require('../models/Publisher')
 
 const connection = new Sequelize(configDB)
 
 Category.init(connection)
+Publisher.init(connection)
 
 module.exports = connection
