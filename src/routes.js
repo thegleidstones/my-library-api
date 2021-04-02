3bf816efffe47ec6c07028e1868a1fc212a3af3b
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-04-01 23:01:18 -0300

    feat: add authors routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router()
 
 const categoryController = require('./controllers/CategoryController')
 const publisherController = require('./controllers/PublisherController')
+const authorController = require('./controllers/AuthorController')
 
 routes.get('/categories', categoryController.list)
 routes.post('/categories', categoryController.create)
@@ -11,5 +12,9 @@ routes.delete('/categories/:categoryId', categoryController.delete)
 routes.get('/publishers', publisherController.list)
 routes.post('/publishers', publisherController.create)
 routes.delete('/publishers/:publisherId', publisherController.delete)
+
+routes.get('/authors', authorController.list)
+routes.post('/authors', authorController.create)
+routes.delete('/authors/:authorId', authorController.delete)
 
 module.exports = routes
