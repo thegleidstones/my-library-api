1f68921316261e9ff50100785d5401ce10d3f39a
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-04-01 23:30:43 -0300

    feat: init model Friend

--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize')
 const configDB = require('../config/database')
 const Author = require('../models/Author')
 const Category = require('../models/Category')
+const Friend = require('../models/Friend')
 const Publisher = require('../models/Publisher')
 
 const connection = new Sequelize(configDB)
@@ -9,5 +10,6 @@ const connection = new Sequelize(configDB)
 Category.init(connection)
 Publisher.init(connection)
 Author.init(connection)
+Friend.init(connection)
 
 module.exports = connection
