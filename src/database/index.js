11a1d8b7997d1b576166c883bec78c89bd22655e
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-04-01 23:01:46 -0300

    feat: init model author

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const configDB = require('../config/database')
+const Author = require('../models/Author')
 const Category = require('../models/Category')
 const Publisher = require('../models/Publisher')
 
@@ -7,5 +8,6 @@ const connection = new Sequelize(configDB)
 
 Category.init(connection)
 Publisher.init(connection)
+Author.init(connection)
 
 module.exports = connection
